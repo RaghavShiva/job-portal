32f5d14bf764b2caca42456e02caff3e1b078ab7
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-04-26 13:05:18 +0530

    Update Register.jsx

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://job-portal-5rfd.onrender.com/api/v1/user/register",
+        "https://job-portal-1-qt6d.onrender.com/api/v1/user/register",
         { name, phone, email, role, password },
         {
           headers: {
