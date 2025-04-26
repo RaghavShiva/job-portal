5b73fad7819d08b36a1da6e61d96667b2b8c9def
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-04-26 13:06:28 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("https://job-portal-5rfd.onrender.com/api/v1/job/getall", {
+        .get("https://job-portal-1-qt6d.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
